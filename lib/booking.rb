3158260c9361fb2587d09d56eed5442643fa17f9
DanielleInkster <danielle.inkster@gmail.com>
2019-10-21 17:00:34 +0100

    Started Booking class

---
+++ lib/booking.rb
@@ -0,0 +1,5 @@
+class Booking
+
+  def self.create(user_name:, request_date:, property_id:)
+  end
+end
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -2,6 +2,16 @@ require 'booking'
 
 describe Booking do 
 
-  describe ''
+  describe '#create' do
+    it 'creates a new booking' do
+      booking = Booking.create(user_name: 'Danielle', request_date: 'Date.new', property_id: '1')
+
+      persisted_data = persisted_data(table: :users, id: booking.id)
+  
+      expect(booking).to be_a Booking
+      expect(booking.id).to eq ('1')
+      expect(booking.user_name).to eq 'Danielle'
+    end
+  end
 
 end
