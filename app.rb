4b0e8d16229793b39b7b618d9e75a150307a8f44
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-10-23 11:52:31 +0100

    Refactors request list method into model

--- app.rb
+++ app.rb
@@ -55,17 +55,7 @@ class MakersBnB < Sinatra::Base
   end
 
   post '/requests' do
-    # this will be extracted into the model
-    booking_requests = BookingRequest.where(user_id: session[:user_id])
-    @requests = []
-    booking_requests.each { |br|
-      @requests.push( {
-        listing_name: Listing.find_by(id: br.listing_id).name,
-        guest: User.find_by(id: br.guest).name,
-        requested_date: br.requested_date,
-        br_id: br.id
-        })
-    }
+    @requests = BookingRequest.get_user_bookings(user_id: session[:user_id])
     erb :booking_request_list
   end
 
--- lib/booking_requests.rb
+++ lib/booking_requests.rb
@@ -1,4 +1,21 @@
+require_relative 'listing'
+require_relative 'user'
+
 class BookingRequest < ActiveRecord::Base
   belongs_to :user
   has_many :listings
+
+  def self.get_user_bookings(user_id:)
+    booking_requests = BookingRequest.where(user_id: user_id)
+    requests = []
+    booking_requests.each { |br|
+      requests.push( {
+        listing_name: Listing.find_by(id: br.listing_id).name,
+        guest: User.find_by(id: br.guest).name,
+        requested_date: br.requested_date,
+        br_id: br.id
+      })
+    }
+    requests
+  end
 end
